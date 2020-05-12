3fbc05d17db1a26ed0741b091b28b8bf9fdb56ed
Ubuntu <ubuntu@deleteme.cloud.ut.ee>
2020-05-12 21:01:15 +0000

    formatting updated
    (cherry picked from commit e49082d06e5d6dd54a27b2803bc52378aadb36d0)

--- nmt.py
+++ nmt.py
@@ -15,14 +15,22 @@ from constraints import getPolitenessConstraints as getCnstrs
 from log import log
 
 # IP and port for the server
-MY_IP = 'localhost'
+MY_IP = "localhost"
 MY_PORT = 12346
 
-supportedStyles = { "os", "un", "dg", "jr", "ep", "pc", "em", "nc" }
-supportedOutLangs = { 'et', 'lv', 'en', 'ru', 'fi', 'lt', 'de' }
-extraSupportedOutLangs = { 'est': 'et', 'lav': 'lv', 'eng': 'en', 'rus': 'ru', 'fin': 'fi', 'lit': 'lt', 'ger': 'de' }
-defaultStyle = 'nc'
-defaultOutLang = 'en'
+supportedStyles = {"os", "un", "dg", "jr", "ep", "pc", "em", "nc"}
+supportedOutLangs = {"et", "lv", "en", "ru", "fi", "lt", "de"}
+extraSupportedOutLangs = {
+    "est": "et",
+    "lav": "lv",
+    "eng": "en",
+    "rus": "ru",
+    "fin": "fi",
+    "lit": "lt",
+    "ger": "de",
+}
+defaultStyle = "nc"
+defaultOutLang = "en"
 
 USAGE_MSG = """\nUsage: nmtnazgul.py  translation_model  truecaser_model  segmenter_model [output_lang [output_style]]
 	
@@ -35,7 +43,9 @@ Without the output language and any further parameters an NMT server is started;
 	output_lang: output language (one of the following: {0})
 	output_style: output style (one of the following: {1}; default: {2})
 	
-Further info: http://github.com/tartunlp/nazgul\n\n""".format(", ".join(list(supportedOutLangs)), ", ".join(list(supportedStyles)), defaultStyle)
+Further info: http://github.com/tartunlp/nazgul\n\n""".format(
+    ", ".join(list(supportedOutLangs)), ", ".join(list(supportedStyles)), defaultStyle
+)
 
 
 #############################################################################################
@@ -44,99 +54,103 @@ Further info: http://github.com/tartunlp/nazgul\n\n""".format(", ".join(list(sup
 
 
 def getConf(rawConf):
-	style = defaultStyle
-	outlang = defaultOutLang
-	
-	for field in rawConf.split(','):
-		if field in supportedStyles:
-			style = field
-		if field in supportedOutLangs:
-			outlang = field
-		if field in extraSupportedOutLangs:
-			outlang = extraSupportedOutLangs[field]
-	
-	return style, outlang
+    style = defaultStyle
+    outlang = defaultOutLang
+
+    for field in rawConf.split(","):
+        if field in supportedStyles:
+            style = field
+        if field in supportedOutLangs:
+            outlang = field
+        if field in extraSupportedOutLangs:
+            outlang = extraSupportedOutLangs[field]
+
+    return style, outlang
 
 
 def parseInput(rawText):
-	global supportedStyles, defaultStyle, supportedOutLangs, defaultOutLang
-	
-	try:
-		fullText = rawText['src']
-		rawStyle, rawOutLang = getConf(rawText['conf'])
-		
-		livesubs = "|" in fullText
-		
-		sentences = fullText.split("|") if livesubs else sent_tokenize(fullText)
-		delim = "|" if livesubs else " "
-		
-	except KeyError:
-		sentences = rawText['sentences']
-		rawStyle = rawText['outStyle']
-		rawOutLang = rawText['outLang']
-		delim = False
-	
-	if rawStyle not in supportedStyles:
-		#raise ValueError("style bad: " + rawStyle)
-		rawStyle = defaultStyle
-	
-	if rawOutLang not in supportedOutLangs:
-		#raise ValueError("out lang bad: " + rawOutLang)
-		rawOutLang = defaultOutLang
-	
-	outputLang = rawOutLang
-	outputStyle = rawStyle
-	
-	return sentences, outputLang, outputStyle, delim
+    global supportedStyles, defaultStyle, supportedOutLangs, defaultOutLang
+
+    try:
+        fullText = rawText["src"]
+        rawStyle, rawOutLang = getConf(rawText["conf"])
+
+        livesubs = "|" in fullText
+
+        sentences = fullText.split("|") if livesubs else sent_tokenize(fullText)
+        delim = "|" if livesubs else " "
+
+    except KeyError:
+        sentences = rawText["sentences"]
+        rawStyle = rawText["outStyle"]
+        rawOutLang = rawText["outLang"]
+        delim = False
+
+    if rawStyle not in supportedStyles:
+        # raise ValueError("style bad: " + rawStyle)
+        rawStyle = defaultStyle
+
+    if rawOutLang not in supportedOutLangs:
+        # raise ValueError("out lang bad: " + rawOutLang)
+        rawOutLang = defaultOutLang
+
+    outputLang = rawOutLang
+    outputStyle = rawStyle
+
+    return sentences, outputLang, outputStyle, delim
 
 
 def decodeRequest(rawMessage):
-	struct = json.loads(rawMessage.decode('utf-8'))
-	
-	segments, outputLang, outputStyle, delim = parseInput(struct)
-	
-	return segments, outputLang, outputStyle, delim
+    struct = json.loads(rawMessage.decode("utf-8"))
+
+    segments, outputLang, outputStyle, delim = parseInput(struct)
+
+    return segments, outputLang, outputStyle, delim
 
 
 def encodeResponse(translationList, delim):
-	translationText = delim.join(translationList)
-	
-	result = json.dumps({'raw_trans': ['-'],
-		'raw_input': ['-'],
-		'final_trans': translationText})
-	
-	return bytes(result, 'utf-8')
+    translationText = delim.join(translationList)
+
+    result = json.dumps(
+        {"raw_trans": ["-"], "raw_input": ["-"], "final_trans": translationText}
+    )
+
+    return bytes(result, "utf-8")
 
 
 def serverTranslationFunc(rawMessage, models):
-	segments, outputLang, outputStyle, delim = decodeRequest(rawMessage)
-	
-	translations, _, _, _ = translator.translate(models, segments, outputLang, outputStyle, getCnstrs())
-	
-	return encodeResponse(translations, delim)
+    segments, outputLang, outputStyle, delim = decodeRequest(rawMessage)
+
+    translations, _, _, _ = translator.translate(
+        models, segments, outputLang, outputStyle, getCnstrs()
+    )
+
+    return encodeResponse(translations, delim)
 
 
 def startTranslationServer(models, ip, port):
-	log("started server")
-	
-	# start listening as a socket server; apply serverTranslationFunc to incoming messages to genereate the response
-	sock.startServer(serverTranslationFunc, (models,), port = port, host = ip)
+    log("started server")
+
+    # start listening as a socket server; apply serverTranslationFunc to incoming messages to genereate the response
+    sock.startServer(serverTranslationFunc, (models,), port=port, host=ip)
 
 
 def translateStdinInBatches(models, outputLang, outputStyle):
-	"""Read lines from STDIN and treat each as a segment to translate;
+    """Read lines from STDIN and treat each as a segment to translate;
 	translate them and print out tab-separated scores (decoder log-prob)
 	and the translation outputs"""
-	
-	#read STDIN as a list of segments
-	lines = [line.strip() for line in sys.stdin]
-	
-	#translate segments and get translations and scores
-	translations, scores, _, _ = translator.translate(models, lines, outputLang, outputStyle, getCnstrs())
-	
-	#print each score and translation, separated with a tab
-	for translation, score in zip(translations, scores):
-		print("{0}\t{1}".format(score, translation))
+
+    # read STDIN as a list of segments
+    lines = [line.strip() for line in sys.stdin]
+
+    # translate segments and get translations and scores
+    translations, scores, _, _ = translator.translate(
+        models, lines, outputLang, outputStyle, getCnstrs()
+    )
+
+    # print each score and translation, separated with a tab
+    for translation, score in zip(translations, scores):
+        print("{0}\t{1}".format(score, translation))
 
 
 #############################################################################################
@@ -145,55 +159,55 @@ def translateStdinInBatches(models, outputLang, outputStyle):
 
 
 def readCmdlineModels():
-	"""Read translation, truecaser and segmenter model paths from cmdline;
+    """Read translation, truecaser and segmenter model paths from cmdline;
 	show usage info if failed"""
-	
-	#This is a quick hack for reading cmdline args, should use argparse instead
-	try:
-		translationModelPath = sys.argv[1]
-		truecaserModelPath = sys.argv[2]
-		segmenterModelPath = sys.argv[3]
-	except IndexError:
-		sys.stderr.write(USAGE_MSG)
-		sys.exit(-1)
-	
-	return translationModelPath, truecaserModelPath, segmenterModelPath
+
+    # This is a quick hack for reading cmdline args, should use argparse instead
+    try:
+        translationModelPath = sys.argv[1]
+        truecaserModelPath = sys.argv[2]
+        segmenterModelPath = sys.argv[3]
+    except IndexError:
+        sys.stderr.write(USAGE_MSG)
+        sys.exit(-1)
+
+    return translationModelPath, truecaserModelPath, segmenterModelPath
 
 
 def readLangAndStyle():
-	"""Read output language and style off cmdline.
+    """Read output language and style off cmdline.
 	Language is optional -- if not given, a server is started.
 	Style is optional -- if not given, default (auto) is used."""
-	
-	# EAFP
-	try:
-		outputLanguage = sys.argv[4]
-		
-		try:
-			outputStyle = sys.argv[5]
-		except IndexError:
-			outputStyle = defaultStyle
-	
-	except IndexError:
-		outputLanguage = None
-		outputStyle = None
-	
-	return outputLanguage, outputStyle
+
+    # EAFP
+    try:
+        outputLanguage = sys.argv[4]
+
+        try:
+            outputStyle = sys.argv[5]
+        except IndexError:
+            outputStyle = defaultStyle
+
+    except IndexError:
+        outputLanguage = None
+        outputStyle = None
+
+    return outputLanguage, outputStyle
 
 
 if __name__ == "__main__":
-	# read translation and preprocessing model paths off cmdline
-	modelPaths = readCmdlineModels()
-	
-	# read output language and style off cmdline -- both are optional and will be "None" if not given
-	olang, ostyle = readLangAndStyle()
-	
-	# load translation and preprocessing models using paths
-	models = translator.loadModels(*modelPaths)
-	
-	# if language is given, STDIN is translated; otherwise a server is started
-	if olang:
-		translateStdinInBatches(models, olang, ostyle)
-	else:
-		# when argparse is finally used, set MY_IP and MY_PORT to cmdline arguments
-		startTranslationServer(models, MY_IP, MY_PORT)
+    # read translation and preprocessing model paths off cmdline
+    modelPaths = readCmdlineModels()
+
+    # read output language and style off cmdline -- both are optional and will be "None" if not given
+    olang, ostyle = readLangAndStyle()
+
+    # load translation and preprocessing models using paths
+    models = translator.loadModels(*modelPaths)
+
+    # if language is given, STDIN is translated; otherwise a server is started
+    if olang:
+        translateStdinInBatches(models, olang, ostyle)
+    else:
+        # when argparse is finally used, set MY_IP and MY_PORT to cmdline arguments
+        startTranslationServer(models, MY_IP, MY_PORT)
--- truecaser/applytc.py
+++ truecaser/applytc.py
@@ -5,88 +5,102 @@ import re
 
 from truecaser.learntc import log, tokens
 
+
 class DefUniqDict(dict):
-	def __missing__(self, key):
-		return key
+    def __missing__(self, key):
+        return key
 
-class WordFreqTuple():
-	def __init__(self, word, freq):
-		self.word = word
-		self.freq = freq
 
-def loadModel(filename, freqs = False):
-	res = DefUniqDict()
-	
-	with open(filename, 'r') as filehandle:
-		for l in filehandle:
-			try:
-				w, f = l.strip().split('\t')
-			except:
-				#sys.stderr.write(l)
-				#raise e
-				w = l.strip()
-				f = 5
-			
-			res[w.lower()] = WordFreqTuple(w, int(f))
-		
-		return res
+class WordFreqTuple:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+
+
+def loadModel(filename, freqs=False):
+    res = DefUniqDict()
+
+    with open(filename, "r") as filehandle:
+        for l in filehandle:
+            try:
+                w, f = l.strip().split("\t")
+            except:
+                # sys.stderr.write(l)
+                # raise e
+                w = l.strip()
+                f = 5
+
+            res[w.lower()] = WordFreqTuple(w, int(f))
+
+        return res
+
 
 def isUpper(w):
-	return re.search(r'[A-Z]', w) and not re.search(r'[a-z]', w)
+    return re.search(r"[A-Z]", w) and not re.search(r"[a-z]", w)
+
 
 def truecase(model, wordlist):
-	return [model[w.lower()].word if (w.lower() in model and (i == 0 or isUpper(w) or wordlist[i-1] in ".:;?!")) else w for i, w in enumerate(wordlist)]
+    return [
+        model[w.lower()].word
+        if (w.lower() in model and (i == 0 or isUpper(w) or wordlist[i - 1] in ".:;?!"))
+        else w
+        for i, w in enumerate(wordlist)
+    ]
+
 
 def updateToken(line, span, newtoken):
-	return line[:span[0]] + newtoken + line[span[1]:]
+    return line[: span[0]] + newtoken + line[span[1] :]
+
 
 def processLine(model, line):
-	try:
-		toks = tokens(line)
-		words, spans = zip(*toks)
+    try:
+        toks = tokens(line)
+        words, spans = zip(*toks)
 
-		tcwords = truecase(model, words)
+        tcwords = truecase(model, words)
 
-		resline = line
+        resline = line
 
-		for w, s in zip(tcwords, spans):
-			resline = updateToken(resline, s, w)
+        for w, s in zip(tcwords, spans):
+            resline = updateToken(resline, s, w)
 
-		return resline
-	except:
-		return line
+        return resline
+    except:
+        return line
+
 
 def processLines(model, fh):
-	logFreq = 100000
-	i = 0
+    logFreq = 100000
+    i = 0
 
-	for line in fh:
-		try:
-			print(processLine(model, line.strip()))
-		except ValueError as e:
-			sys.stderr.write("empty\n")
-			print("")
-			pass
+    for line in fh:
+        try:
+            print(processLine(model, line.strip()))
+        except ValueError as e:
+            sys.stderr.write("empty\n")
+            print("")
+            pass
 
-		i += 1
-		if not i % logFreq:
-			log("processed {0} lines".format(i))
+        i += 1
+        if not i % logFreq:
+            log("processed {0} lines".format(i))
 
-	if i % logFreq:
-		log("processed {0} lines".format(i))
+    if i % logFreq:
+        log("processed {0} lines".format(i))
 
-if __name__ == '__main__':
-	modelfile = sys.argv[1]
-	
-	model = loadModel(modelfile)
-	
-	try:
-		filename = sys.argv[2]
-	except IndexError:
-		filename = '-'
-	
-	if filename == '-':
-		processLines(model, sys.stdin)
-	else:
-		with open(filename, 'r') as fh:
-			processLines(model, fh)
+
+if __name__ == "__main__":
+    modelfile = sys.argv[1]
+
+    model = loadModel(modelfile)
+
+    try:
+        filename = sys.argv[2]
+    except IndexError:
+        filename = "-"
+
+    if filename == "-":
+        processLines(model, sys.stdin)
+    else:
+        with open(filename, "r") as fh:
+            processLines(model, fh)
