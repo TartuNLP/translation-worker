076a2979d010fed2cfd275fd7882de8ca7b15f9f
Liisa Rätsep <liisa.ratsep@ut.ee>
2021-03-11 16:24:58 +0200

    initial commit

---
+++ nmt_service.py
@@ -0,0 +1,103 @@
+import itertools
+import logging
+
+from typing import Dict, Any, Optional
+
+from nltk import sent_tokenize
+from marshmallow import Schema, fields, validate, ValidationError
+import pika
+
+from nauron import Response, Service, MQConsumer
+
+import settings
+from translator import Translator, parse_domains
+
+
+logger = logging.getLogger("nmt_service")
+
+class TranslationService(Service):
+    def __init__(self, request_schema: Schema(), engine: Translator, char_limit: int = 10000):
+        self.schema = request_schema
+        self.char_limit = char_limit
+        # Load model
+        self.engine = engine
+
+    def process_request(self, body: Dict[str, Any], _: Optional[str] = None) -> Response:
+        try:
+            body = self.schema().load(body)
+        except ValidationError as error:
+            return Response(content=error.messages, http_status_code=400)
+
+        delimiters = None
+        if type(body['text']) == str:
+            text = body['text']
+            sentences = sent_tokenize(body['text'])
+            try:
+                delimiters = []
+                for sent in sentences:
+                    idx = text.index(sent)
+                    delimiters.append(text[:idx])
+                    text = text[idx + len(sent):]
+                delimiters.append(text)
+            except ValueError:
+                delimiters = ['', *[' ' for _ in range(len(sentences)-1)], '']
+        else:
+            sentences = body['text']
+
+        length = sum([len(sent) for sent in sentences])
+
+        if length == 0:
+            if type(body['text']) == str:
+                return Response({'result': ""}, mimetype="application/json")
+            else:
+                return Response({'result': []}, mimetype="application/json")
+        elif length > self.char_limit:
+            return Response(content=f"Request is too large ({length} characters). "
+                                    f"Maximum request size is {self.char_limit} characters.",
+                            http_status_code=413)
+        else:
+            sent_factors = {'lang': self.engine.factors['lang']['mapping'][body['tgt']]}
+            if 'domain' in self.engine.factors:
+                if body['domain'] and body['domain'] in self.engine.factors['domain']['mapping']:
+                    sent_factors['domain'] = self.engine.factors['domain']['mapping'][body['domain']]
+                else:
+                    sent_factors['domain'] = self.engine.factors['domain']['factors'][0]
+            translations, _, _, _ = self.engine.translate(sentences, sent_factors)
+            if delimiters:
+                translations = ''.join(itertools.chain.from_iterable(zip(delimiters, translations)))+delimiters[-1]
+
+            return Response({'result': translations}, mimetype="application/json")
+
+
+
+if __name__ == "__main__":
+    mq_parameters = pika.ConnectionParameters \
+        (host=settings.MQ_HOST, port=settings.MQ_PORT,
+         credentials=pika.credentials.PlainCredentials(username=settings.MQ_USERNAME,
+                                                       password=settings.MQ_PASSWORD))
+
+    factors = parse_domains({factor:settings.FACTORS.setdefault(factor, ['{}0'.format(factor)]) for
+                             factor in settings.FACTOR_SEQUENCE})
+    factors["sequence"] = settings.FACTOR_SEQUENCE
+    for factor in settings.FACTOR_SEQUENCE:
+        factors[factor]['mapping'] = {**settings.ALT_FACTORS.setdefault(factor, {}),
+                                      **{v:v for v in factors[factor]['factors']}}
+
+    logger.info("Factors: {}".format(factors))
+    translation_engine = Translator(settings.NMT_MODEL, settings.SPM_MODEL, settings.TC_MODEL, settings.CPU, factors)
+    logger.info("All models loaded")
+
+    class NMTSchema(Schema):
+        text = fields.Raw(validate=(lambda obj: type(obj) in [str, list]))
+        src = fields.Str()
+        tgt = fields.Str(missing=factors['lang']['factors'][0],
+                         validate=validate.OneOf(factors['lang']['mapping'].keys()))
+        domain = fields.Str(missing="")
+
+
+    service = MQConsumer(service=TranslationService(NMTSchema, translation_engine, settings.CHAR_LIMIT),
+                         connection_parameters=mq_parameters,
+                         exchange_name=settings.MQ_EXCHANGE,
+                         queue_name=settings.MQ_QUEUE_NAME,
+                         alt_routes=settings.MQ_ALT_ROUTES)
+    service.start()
---
+++ translator.py
@@ -0,0 +1,129 @@
+"""Sockeye model loading and inference"""
+import json
+import logging
+
+from typing import List
+
+import mxnet as mx
+import sentencepiece as spm
+from sockeye.translate import inference
+
+from truecaser_ import applytc
+
+
+logger = logging.getLogger("nmt_service")
+
+def parse_domains(factors):
+    parsed_factors = {}
+    for k, v in factors.items():
+        if v:
+            _tmp = {"factors": v}
+            if len(v) == 1:
+                _tmp["used"] = False
+            parsed_factors[k] = _tmp
+    return parsed_factors
+
+class Translator:
+    models = None
+    spm_model = spm.SentencePieceProcessor()
+    tc_model = None
+
+    def __init__(self, sockeye_model_folder: str, spm_model_path: str, tc_model_path: str, use_cpu: bool,
+                 factors: dict):
+        self.mx_ctx = mx.cpu() if use_cpu else mx.gpu()
+        self.use_truecaser = True if tc_model_path is not None else False
+        self.constrains = None  # Legacy thing
+        self.factors = factors
+        self.load_models(sockeye_model_folder, spm_model_path, tc_model_path)
+
+
+    def _preprocess(self, sent: str, sent_factors: dict):
+        sentence = applytc.processLine(self.tc_model, sent) if self.tc_model else sent
+
+        reformat_sentence = " ".join(self.spm_model.encode(sentence, out_type=str))
+
+        n_words = len(reformat_sentence.split())
+
+        # Prepare factors
+        factors = []
+        if len(self.factors["sequence"]) > 0:
+            for fs in self.factors["sequence"]:
+                if fs in sent_factors:
+                    factors.append(" ".join([sent_factors[fs]] * n_words))
+                else:
+                    factors.append(" ".join([f"{fs}0"] * n_words))
+
+        preproc_json = {"text": reformat_sentence, "factors": factors}
+        logger.info(f"Preprocessed: {sent} into {reformat_sentence}.")
+        return json.dumps(preproc_json)
+
+    def _postprocess(self, sent: str):
+        postproc_sent = self.spm_model.DecodePieces(sent.split())
+        postproc_sent = postproc_sent.replace("▁", " ").split()
+
+        # Do truecasing
+        postproc_sent[0] = postproc_sent[0].capitalize()
+
+        res = " ".join(postproc_sent)
+
+        logger.info(f"Postprocessed: {sent} into: {res}.")
+        return res
+
+    def _forward(self, sents: List[str]):
+        translation_inputs = [
+            inference.make_input_from_json_string(sentence_id=i, json_string=sent, translator=self.models)
+            for (i, sent) in enumerate(sents)]
+        outputs = self.models.translate(translation_inputs)
+        return [(output.translation, output.score) for output in outputs]
+
+    def translate(self, sents: List[str], sent_factors: dict):
+        inputs = [self._preprocess(sent, sent_factors) for sent in sents]
+
+        scored_translations = self._forward(inputs)
+
+        translations, scores = zip(*scored_translations)
+
+        postproc_translations = [self._postprocess(sent) for sent in translations]
+        return postproc_translations, scores, inputs, translations
+
+    def load_models(self, sockeye_model_folder: str, spm_model_path: str, tc_model_path: str):
+        """Load translation and segmentation models."""
+
+        self.models = load_sockeye_v1_translator_models([sockeye_model_folder, ], self.mx_ctx)
+        self.spm_model.Load(spm_model_path)
+        if tc_model_path:
+            self.tc_model = applytc.loadModel(tc_model_path)
+
+
+def load_sockeye_v1_translator_models(model_folders, ctx=mx.gpu()):
+    logger.info(f"Loading Sockeye models. MXNET context: {ctx}")
+    models, source_vocabs, target_vocab = inference.load_models(
+        context=ctx,
+        max_input_len=None,
+        beam_size=3,
+        batch_size=16,
+        model_folders=model_folders,
+        checkpoints=None,
+        softmax_temperature=None,
+        max_output_length_num_stds=2,
+        decoder_return_logit_inputs=False,
+        cache_output_layer_w_b=False)
+
+    return inference.Translator(context=ctx,
+                                ensemble_mode="linear",
+                                bucket_source_width=10,
+                                length_penalty=inference.LengthPenalty(1.0, 0.0),
+                                beam_prune=0,
+                                beam_search_stop='all',
+                                models=models,
+                                source_vocabs=source_vocabs,
+                                target_vocab=target_vocab,
+                                restrict_lexicon=None,
+                                store_beam=False,
+                                strip_unknown_words=False)
+
+
+
+
+
+
